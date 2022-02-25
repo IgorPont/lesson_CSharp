1f1e58d5c6765636c9027cb57b27cb2f88fdd638
igor_pont <pontigor11@gmail.com>
2022-02-25 10:46:42 +0300

    add example 006 max

--- example006_Base/Program.cs
+++ example006_Base/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int a = 37;
+int b = 7;
+int c = 10;
+int d = 32;
+int e = 1;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальное число равно ");
+Console.WriteLine(max);
